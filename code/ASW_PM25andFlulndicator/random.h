93b52b9b316460f57612505196e4419c07e7869a
Sirapol Nokyoonthong <pordeetalent@gmail.com>
2021-06-25 23:23:12 +0700

    rename fzz flie

--- code/ASW_PM25andFlulndicator/random.h
+++
@@ -1,9 +0,0 @@
-#ifndef __RANDOM_H
-#define __RANDOM_H
-
-extern char ClientID[24];
-
-extern void set_wifi(void);
-extern char generateClientID (void); 
-
-#endif
--- code/AWS_PM25andFlulndicator/global.h
+++
@@ -1,7 +0,0 @@
-#ifndef __GLOBAL_H
-#define __GLOBAL_H
-
-#define WLAN_SSID "SunNET"       
-#define WLAN_PASS "maramara"
-
-#endif
